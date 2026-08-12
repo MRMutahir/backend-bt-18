cdf24bb83976ff8f2f5d44331f76e3fb0c9ee00e
MRMutahir <mutahirkareem820@gmail.com>
2026-08-12 07:18:27 +0500

    access token complete

---
+++ class10/server.js
@@ -0,0 +1,29 @@
+import { configDotenv } from "dotenv";
+configDotenv()
+import express from "express";
+import { connectDB } from "./config/db.js";
+import { User } from "./Model/Users.Model.js";
+import { AuthRoutes } from "./Routes/Auth.js";
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Server is running",
+    });
+});
+
+app.use('/v1/auth', AuthRoutes)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
